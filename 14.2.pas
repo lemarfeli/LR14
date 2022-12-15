6fc88ee6e26da87fed81ff91958a91bf566c7b81
feli <117822922+lemarfeli@users.noreply.github.com>
2022-12-15 08:10:16 +0300

    Add files via upload

---
+++ 14.2.pas
@@ -0,0 +1,28 @@
+﻿Var f1,f2:text;
+    s,max,s1:string;
+    i:byte;
+Begin
+    assign(f1,'input.txt');reset(f1);
+    max:='';
+    while not eof(f1) do
+    begin
+      readln(f1,s);
+      if length(s)>length(max) then max:=s;
+    end;
+    close(f1);reset(f1);
+    assign(f2,'output.txt');
+    rewrite(f2);
+    while not eof(f1) do
+    begin
+      readln(f1,s);
+      if length(s)=length(max) then
+      begin
+        s1:='';
+        for i:=1 to length(s) do
+         s1:=s[i]+s1;
+        writeln(f2,s1);
+      end;
+    end;
+    close(f1);close(f2);
+    readln;
+End.
