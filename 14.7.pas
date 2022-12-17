cbbc96523cefa60b92de3d8be8d8a15f4dc78fb9
feli <117822922+lemarfeli@users.noreply.github.com>
2022-12-17 10:42:39 +0300

    Add files via upload

---
+++ 14.7.pas
@@ -0,0 +1,16 @@
+﻿begin
+var f: file of char;
+var c:='!';
+var n:=1;
+    assign (f, 'c:\e.txt');
+    reset (f);
+  while (not eof(f)) do
+   begin
+   if n mod 2<>0 then begin
+   seek(f,n);
+   write(f,c);
+   end;
+   n:=n+1;
+   end;
+    close(f);
+ end.
